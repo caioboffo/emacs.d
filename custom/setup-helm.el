876ee4dc297b19b699e423e11491929cd387eb27
Caio B. Silva <caioboffo@gmail.com>
2020-04-25 09:25:41 -0300

    Comment helm config something is preventing M-x to choose commands
    Signed-off-by: Caio B. Silva <caioboffo@gmail.com>

--- custom/setup-helm.el
+++ custom/setup-helm.el
@@ -37,38 +37,38 @@
     (when (executable-find "curl")
       (setq helm-google-suggest-use-curl-p t))
 
-    (setq helm-google-suggest-use-curl-p t
-          helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
-          ;; helm-quick-update t ; do not display invisible candidates
-          helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+    ;; (setq helm-google-suggest-use-curl-p t
+    ;;       helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
+    ;;       ;; helm-quick-update t ; do not display invisible candidates
+    ;;       helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
 
-          ;; you can customize helm-do-grep to execute ack-grep
-          ;; helm-grep-default-command "ack-grep -Hn --smart-case --no-group --no-color %e %p %f"
-          ;; helm-grep-default-recurse-command "ack-grep -H --smart-case --no-group --no-color %e %p %f"
-          helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+    ;;       ;; you can customize helm-do-grep to execute ack-grep
+    ;;       ;; helm-grep-default-command "ack-grep -Hn --smart-case --no-group --no-color %e %p %f"
+    ;;       ;; helm-grep-default-recurse-command "ack-grep -H --smart-case --no-group --no-color %e %p %f"
+    ;;       helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
 
-          helm-echo-input-in-header-line t
+    ;;       helm-echo-input-in-header-line t
 
-          ;; helm-candidate-number-limit 500 ; limit the number of displayed canidates
-          helm-ff-file-name-history-use-recentf t
-          helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
-          helm-buffer-skip-remote-checking t
+    ;;       ;; helm-candidate-number-limit 500 ; limit the number of displayed canidates
+    ;;       helm-ff-file-name-history-use-recentf t
+    ;;       helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
+    ;;       helm-buffer-skip-remote-checking t
 
-          helm-mode-fuzzy-match t
+    ;;       helm-mode-fuzzy-match t
 
-          helm-buffers-fuzzy-matching t ; fuzzy matching buffer names when non-nil
-                                        ; useful in helm-mini that lists buffers
-          helm-org-headings-fontify t
-          ;; helm-find-files-sort-directories t
-          ;; ido-use-virtual-buffers t
-          helm-semantic-fuzzy-match t
-          helm-M-x-fuzzy-match t
-          helm-imenu-fuzzy-match t
-          helm-lisp-fuzzy-completion t
-          ;; helm-apropos-fuzzy-match t
-          helm-buffer-skip-remote-checking t
-          helm-locate-fuzzy-match t
-          helm-display-header-line nil)
+    ;;       helm-buffers-fuzzy-matching t ; fuzzy matching buffer names when non-nil
+    ;;                                     ; useful in helm-mini that lists buffers
+    ;;       helm-org-headings-fontify t
+    ;;       ;; helm-find-files-sort-directories t
+    ;;       ;; ido-use-virtual-buffers t
+    ;;       helm-semantic-fuzzy-match t
+    ;;       helm-M-x-fuzzy-match t
+    ;;       helm-imenu-fuzzy-match t
+    ;;       helm-lisp-fuzzy-completion t
+    ;;       ;; helm-apropos-fuzzy-match t
+    ;;       helm-buffer-skip-remote-checking t
+    ;;       helm-locate-fuzzy-match t
+    ;;       helm-display-header-line nil)
 
     (global-set-key (kbd "M-x") 'helm-M-x)
     (global-set-key (kbd "C-x b") 'helm-buffers-list)
