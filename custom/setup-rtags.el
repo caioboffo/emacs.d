480d6ebda560d9fc6d720e58f84c67828c229d2e
Caio Silva <caio.silva@datacom.ind.br>
2020-05-08 11:33:55 -0300

    disable hotkey for rtags-find-virtuals-at-point

--- custom/setup-rtags.el
+++ custom/setup-rtags.el
@@ -27,8 +27,8 @@
   (require 'company)
   (define-key c-mode-base-map (kbd "M-.")
 	(function rtags-find-symbol-at-point))
-  (define-key c-mode-base-map (kbd "M-,")
-	(function rtags-find-references-at-point))
+  ;; (define-key c-mode-base-map (kbd "M-;")
+  ;;   (function rtags-find-virtuals-at-point))
   ;; disable prelude's use of C-c r, as this is the rtags keyboard prefix
   ;;(define-key prelude-mode-map (kbd "C-c r") nil)
   ;; install standard rtags keybindings. Do M-. on the symbol below to
