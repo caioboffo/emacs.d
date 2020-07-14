d792d05129e77d1b7f54d3e9fae94d4455244b6b
Caio B. Silva <caioboffo@gmail.com>
2020-07-14 08:16:19 -0300

    revert experimentng with helm and add key to helm-occur
    Signed-off-by: Caio B. Silva <caioboffo@gmail.com>

--- custom/setup-helm.el
+++ custom/setup-helm.el
@@ -1,82 +1,156 @@
-;;; setup-helm.el --- Helm config
-;;; Commentary:
-;;; Code:
-;;; Enable Modes (helm-mode is loading nearly everything).
-;;
-(use-package helm-mode
+(use-package helm
+  :ensure t
   :init
-  (add-hook 'helm-mode-hook
-            (lambda ()
-              (setq completion-styles
-                    (cond ((assq 'helm-flex completion-styles-alist)
-                           '(helm-flex))
-                          ((assq 'flex completion-styles-alist)
-                           '(flex))))))
-  :config
-  ;;; Global-map
-  ;;
-  ;;
-  (global-set-key (kbd "M-x")                          'undefined)
-  (global-set-key (kbd "M-x")                          'helm-M-x)
-  (global-set-key (kbd "M-y")                          'helm-show-kill-ring)
-  (global-set-key (kbd "C-x C-f")                      'helm-find-files)
-  (global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
-  (global-set-key [remap bookmark-jump]                'helm-filtered-bookmarks)
-  (global-set-key (kbd "C-:")                          'helm-eval-expression-with-eldoc)
-  (global-set-key (kbd "C-,")                          'helm-calcul-expression)
-  (global-set-key (kbd "C-h d")                        'helm-info-at-point)
-  (global-set-key (kbd "C-h i")                        'helm-info)
-  (global-set-key (kbd "C-x C-d")                      'helm-browse-project)
-  (global-set-key (kbd "<f1>")                         'helm-resume)
-  (global-set-key (kbd "C-h C-f")                      'helm-apropos)
-  (global-set-key (kbd "C-h a")                        'helm-apropos)
-  (global-set-key (kbd "C-h C-d")                      'helm-debug-open-last-log)
-  (global-set-key (kbd "<f5> s")                       'helm-find)
-  (global-set-key (kbd "S-<f2>")                       'helm-execute-kmacro)
-  (global-set-key (kbd "C-c i")                        'helm-imenu-in-all-buffers)
-  (global-set-key (kbd "C-c C-i")                      'helm-imenu)
-  (global-set-key (kbd "<f11>")                        nil)
-  (global-set-key (kbd "<f11> o")                      'helm-org-agenda-files-headings)
-  (global-set-key (kbd "C-s")                          'helm-occur)
-  (global-set-key (kbd "<f6> h")                       'helm-emms)
-  (define-key global-map [remap jump-to-register]      'helm-register)
-  (define-key global-map [remap list-buffers]          'helm-mini)
-  (define-key global-map [remap dabbrev-expand]        'helm-dabbrev)
-  (define-key global-map [remap find-tag]              'helm-etags-select)
-  (define-key global-map [remap xref-find-definitions] 'helm-etags-select)
-  (define-key global-map (kbd "M-g a")                 'helm-do-grep-ag)
-  (define-key global-map (kbd "M-g g")                 'helm-grep-do-git-grep)
-  (define-key global-map (kbd "M-g i")                 'helm-gid)
-  (define-key global-map (kbd "C-x r p")               'helm-projects-history)
-  (define-key global-map (kbd "C-x r c")               'helm-addressbook-bookmarks)
-  (define-key global-map (kbd "C-c t r")               'helm-dictionary)
-  (define-key helm-map (kbd "<tab>")                   'helm-execute-persistent-action)
-  (helm-mode 1))
+  (progn
+    (require 'helm-config)
+    (require 'helm-grep)
+    ;; To fix error at compile:
+    ;; Error (bytecomp): Forgot to expand macro with-helm-buffer in
+    ;; (with-helm-buffer helm-echo-input-in-header-line)
+    (if (version< "26.0.50" emacs-version)
+        (eval-when-compile (require 'helm-lib)))
 
-(use-package helm-adaptive
-  :config
-  (setq helm-adaptive-history-file nil)
-  (helm-adaptive-mode 1))
+    (defun helm-hide-minibuffer-maybe ()
+      (when (with-helm-buffer helm-echo-input-in-header-line)
+        (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+          (overlay-put ov 'window (selected-window))
+          (overlay-put ov 'face (let ((bg-color (face-background 'default nil)))
+                                  `(:background ,bg-color :foreground ,bg-color)))
+          (setq-local cursor-type nil))))
 
-(use-package helm-sys
-  :commands (helm-top)
-  :config (helm-top-poll-mode 1))
+    (add-hook 'helm-minibuffer-set-up-hook 'helm-hide-minibuffer-maybe)
+    ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+    ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+    ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+    (global-set-key (kbd "C-c h") 'helm-command-prefix)
+    (global-set-key (kbd "C-x b") 'helm-mini)
+    (global-unset-key (kbd "C-x c"))
 
-(use-package helm-info
-  :bind ("C-h r" . helm-info-emacs))
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+    (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
-(use-package helm-ring
-  :config
-  ;; Action for helm kill-ring
-  (defun helm/emamux:copy-from-kill-ring (candidate)
-    (require 'emamux)
-    (emamux:check-tmux-running)
-    (when (null kill-ring)
-      (error "kill-ring is nil!!"))
-    (emamux:set-buffer candidate 0))
-  (add-to-list 'helm-kill-ring-actions '("Emamux copy" . helm/emamux:copy-from-kill-ring) t)
-  :bind (:map helm-kill-ring-map
-              ("C-d" . helm-kill-ring-run-persistent-delete)))
+    (define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
+    (define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
+    (define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
 
+    (when (executable-find "curl")
+      (setq helm-google-suggest-use-curl-p t))
+
+    ;; (setq helm-google-suggest-use-curl-p t
+    ;;       helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
+    ;;       ;; helm-quick-update t ; do not display invisible candidates
+    ;;       helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+
+    ;;       ;; you can customize helm-do-grep to execute ack-grep
+    ;;       ;; helm-grep-default-command "ack-grep -Hn --smart-case --no-group --no-color %e %p %f"
+    ;;       ;; helm-grep-default-recurse-command "ack-grep -H --smart-case --no-group --no-color %e %p %f"
+    ;;       helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+
+    ;;       helm-echo-input-in-header-line t
+
+    ;;       ;; helm-candidate-number-limit 500 ; limit the number of displayed canidates
+    ;;       helm-ff-file-name-history-use-recentf t
+    ;;       helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
+    ;;       helm-buffer-skip-remote-checking t
+
+    ;;       helm-mode-fuzzy-match t
+
+    ;;       helm-buffers-fuzzy-matching t ; fuzzy matching buffer names when non-nil
+    ;;                                     ; useful in helm-mini that lists buffers
+    ;;       helm-org-headings-fontify t
+    ;;       ;; helm-find-files-sort-directories t
+    ;;       ;; ido-use-virtual-buffers t
+    ;;       helm-semantic-fuzzy-match t
+    ;;       helm-M-x-fuzzy-match t
+    ;;       helm-imenu-fuzzy-match t
+    ;;       helm-lisp-fuzzy-completion t
+    ;;       ;; helm-apropos-fuzzy-match t
+    ;;       helm-buffer-skip-remote-checking t
+    ;;       helm-locate-fuzzy-match t
+    ;;       helm-display-header-line nil)
+
+    (global-set-key (kbd "M-x") 'helm-M-x)
+    (global-set-key (kbd "C-x b") 'helm-buffers-list)
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
+    (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+    (global-set-key (kbd "C-s")     'helm-occur)
+
+    ;;; Save current position to mark ring
+    (add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+
+    (helm-mode 1)
+
+    ))
+
+;; (use-package helm-gtags
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (setq helm-gtags-auto-update t
+;;           helm-gtags-use-input-at-cursor t
+;;           helm-gtags-prefix-key "\C-cg"
+;;           helm-gtags-suggested-key-mapping t))
+
+;;     ;; Enable helm-gtags-mode in Dired so you can jump to any tag
+;;     ;; when navigate project tree with Dired
+;;     (add-hook 'dired-mode-hook 'helm-gtags-mode)
+
+;;     ;; Enable helm-gtags-mode in Eshell for the same reason as above
+;;     (add-hook 'eshell-mode-hook 'helm-gtags-mode)
+
+;;     ;; Enable helm-gtags-mode in languages that GNU Global supports
+;;     (add-hook 'c-mode-hook 'helm-gtags-mode)
+;;     (add-hook 'c++-mode-hook 'helm-gtags-mode)
+;;     (add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+;;     ;; key bindings
+;;     (with-eval-after-load 'helm-gtags
+;;       (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+;;       (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+;;       (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+;;       (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)))
+
+;; Workaround to hide dot files from helm-find-files found in
+;; https://www.reddit.com/r/emacs/comments/5q922h/removing_dot_files_in_helmfindfiles_menu/
+;;
+(require 'cl-lib)
+
+(defvar no-dots-whitelist
+  '()
+  "List of helm buffers in which to show dots.")
+
+(defun no-dots/whitelistedp ()
+  (member (with-helm-buffer (buffer-name)) no-dots-whitelist))
+
+(defun no-dots/helm-ff-filter-candidate-one-by-one (fcn file)
+  (when (or (no-dots/whitelistedp)
+            (not (string-match "\\(?:/\\|\\`\\)\\.\\{1,2\\}\\'" file)))
+    (funcall fcn file)))
+
+(defun no-dots/helm-file-completion-source-p (&rest args) t)
+
+(defun no-dots/helm-attrset (fcn attribute-name value &optional src)
+  (let ((src (or src (helm-get-current-source))))
+    (when src
+      (funcall fcn attribute-name value src))))
+
+(defun no-dots/helm-find-files-up-one-level (fcn &rest args)
+  (advice-add 'helm-file-completion-source-p
+              :around 'no-dots/helm-file-completion-source-p)
+  (advice-add 'helm-attrset
+              :around 'no-dots/helm-attrset)
+  (let ((res (apply fcn args)))
+    (advice-remove 'helm-file-completion-source-p
+                   'no-dots/helm-file-completion-source-p)
+    (advice-remove 'helm-attrset
+                   'no-dots/helm-attrset)
+    res))
+
+(with-eval-after-load 'helm-files
+  (advice-add 'helm-ff-filter-candidate-one-by-one
+              :around 'no-dots/helm-ff-filter-candidate-one-by-one)
+  (advice-add 'helm-find-files-up-one-level
+              :around 'no-dots/helm-find-files-up-one-level))
 
 (provide 'setup-helm)
